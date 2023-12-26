45b0df5fdb5c15159b1aeeea1a05d7d9e8661e03
Nitish1207 <104611554+Nitish1207@users.noreply.github.com>
2023-12-26 08:44:57 +0530

    commiting components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+        <img src="C:\Users\NITISH\projects\LittleLemon\images\Asset 16@4x.png" />
+        </header>
+    );
+        
+}
+
